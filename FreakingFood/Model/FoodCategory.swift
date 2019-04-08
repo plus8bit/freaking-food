ab95b890dfd8483c160b090462e366130de9f33a
Nick <plus0bit@gmail.com>
2019-04-08 17:12:48 +0300

    Add DataModel to the project

---
+++ FreakingFood/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  FreakingFood
+//
+//  Created by Nick on 08/04/2019.
+//  Copyright © 2019 Nick Danilov. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    var title: String
+    var imageName: String
+}
---
+++ FreakingFood/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  FreakingFood
+//
+//  Created by Nick on 08/04/2019.
+//  Copyright © 2019 Nick Danilov. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var instructions: String
+    var imageName: String
+}
