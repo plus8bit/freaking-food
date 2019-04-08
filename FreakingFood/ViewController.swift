1d558292a42605c43573d60e442485a16ee1e1b4
Nick <plus0bit@gmail.com>
2019-04-08 12:35:13 +0300

    Initial xcode project

---
+++ FreakingFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FreakingFood
+//
+//  Created by Nick on 08/04/2019.
+//  Copyright © 2019 Nick Danilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
